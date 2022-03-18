1ac89407f3b93dac18ef5d07f44058f6c1387250
김경석 <drather_dev@naver.com>
2022-03-18 17:43:20 +0900

    add: add Exception, ExcceptionHandler

---
+++ src/main/java/com/tys/openquant/historical/exception/exhandler/ExceptHistoricalDataController.java
@@ -0,0 +1,8 @@
+package com.tys.openquant.historical.exception.exhandler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+public class ExceptHistoricalDataController {
+}
